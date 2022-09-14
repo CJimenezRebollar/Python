d5016b3e512f44fc83c301be30bd7ddd372d8485
CJimenezRebollar <111403266+CJimenezRebollar@users.noreply.github.com>
2022-09-14 08:45:22 -0500

    Update stock.py

--- stock.py
+++ stock.py
@@ -1,24 +1,15 @@
-
-# Imports
 from random import gauss, seed
 from math import sqrt, exp
-
-def create_GBM(s0, mu, sigma):
-# Generates a price following a geometric brownian motion process based on the input of the arguments:
-# - s0: Asset inital price.
-# - mu: Interest rate expressed annual terms.
-# - sigma: Volatility expressed annual terms. 
+def create_GBM(s0, mu, sigma): 
     st = s0
     def generate_value():
         nonlocal st
         st *= exp((mu - 0.5 * sigma ** 2) * (1. / 365.) + sigma * sqrt(1./365.) * gauss(mu=0, sigma=1))
         return st
-
     return generate_value
 if __name__ == "__main__":
     seed(1234)
     gbm = create_GBM(100, 0.1, 0.05)
-
     for _ in range(1000):
         st = gbm()
         print(st)
